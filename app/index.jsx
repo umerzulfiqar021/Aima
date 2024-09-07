552da31464c1e552a64a0fcfebfea73652d2a59a
umerzulfiqar021 <umerzulfiqar38@gmail.com>
2024-09-07 20:09:57 +0500

    solved error

--- app/index.jsx
+++ app/index.jsx
@@ -1,22 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View,SafeAreaView } from 'react-native';
 import { Link } from 'expo-router';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Auraooooaoao</Text>
+    
+    <View  className="flex-1 items-center justify-center">
+      <Text className="text-black-500">Auraooooaoao</Text>
       <StatusBar style="auto" />
       <Link href="/profile" style={{color:'blue'}}>go to Profile</Link>
     </View>
+    
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
