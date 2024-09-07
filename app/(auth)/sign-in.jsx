44e92292ae4e7c2da338b07f3793fcbab175875f
umerzulfiqar021 <umerzulfiqar38@gmail.com>
2024-09-08 01:49:06 +0500

    h-full in view of sign-in file && created component formfield

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,21 +1,28 @@
 import { View, Text, ScrollView, Image} from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import {images} from '../../constants'
+import FormField from '../components/FormField'
+
 const SignIn = () => {
+  const  [form,setForm] = useState ({
+    email : '',
+    password: ''
+  })
   return (
-   <SafeAreaView className = {'bg-primary h-full'}>
+   <SafeAreaView className = {'bg-primary h-full '}>
     <ScrollView>
-      <View className = {'w-full justify-center   px-4 my-6'} >
-    <Image 
-    source={images.logo}
+      <View className = {' w-full justify-center  h-full px-4 my-6'} >
+    <Image source={images.logo}
     resizeMode='contain'
     className = {'w-[115px] h-[35px]'}
     />
-      </View>
-      <Text className = {'text-2xl text-white text-semibold mt-10 font-psemibold'}>
-        Log into Aora
+    <Text className = {'text-2xl text-white text-semibold mt-10 font-psemibold'}>
+        Log in to Aora
         </Text>
+        <FormField/>
+      </View>
+      
     </ScrollView>
    </SafeAreaView>
   )
