cc4c25ed30089d5be14de940be028f55766815d3
umerzulfiqar021 <umerzulfiqar38@gmail.com>
2024-09-08 01:02:43 +0500

    completed main screen

--- app/components/CustomButton.jsx
+++ app/components/CustomButton.jsx
@@ -1,12 +1,21 @@
 import { View, Text,TouchableOpacity } from 'react-native'
 import React from 'react'
 
-const CustomButton = () => {
+const CustomButton = ({title, handlePress, containerStyles, textStyles, isLoading}) => {
   return (
-    <TouchableOpacity className = {`bg-secondary
+    <TouchableOpacity
+    onPress={handlePress}
+    activeOpacity={0.7}
+    className = {`bg-secondary
     rounded-xl min-h-[62px] justify-center items-center
-    `}>
-      <Text>CustomButton</Text>
+    ${containerStyles}
+    ${isLoading ? 'opacity-50' : ''}
+    `}
+    disabled = {isLoading}
+    >
+      <Text className = { `text-primary font-psemibold text-lg  ${textStyles}`}>
+        {title}
+        </Text>
       </TouchableOpacity>
   )
 }
--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View, ScrollView, Image } from 'react-native';
-import { Link } from 'expo-router';
+import { Redirect, router} from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { images } from '../constants'
 import CustomButton from './components/CustomButton';
+
 export default function App() {
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView contentContainerStyle={{ height: '100%' }}>
-        <View className={'w-full justify-center items-center h-full px-4'}>
+        <View className={'w-full justify-center items-center min-h-[85vh] px-4'}>
           <Image
             source={images.logo}
             className={'w-[130px] h-[84px] '}
@@ -34,9 +35,17 @@ export default function App() {
             embark on a journey of limitless exploration
             with Aora
           </Text>
-          <CustomButton/>
+          <CustomButton
+          title = 'Continue with Email'
+          handlePress = { () => {router.push('/sign-in')} }
+          containerStyles = "w-full mt-7"
+          />
         </View>
       </ScrollView>
+      <StatusBar  
+       backgroundColor='#161622' 
+        style='light'
+      />
     </SafeAreaView>
 
 
