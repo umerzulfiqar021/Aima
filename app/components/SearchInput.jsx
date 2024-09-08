8d4666792f2a62e9233b18c011bf44247a4d25d6
umerzulfiqar021 <umerzulfiqar38@gmail.com>
2024-09-09 04:42:06 +0500

    simple modified home screen

---
+++ app/components/SearchInput.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import { View, Text, TextInput, TouchableOpacity, Image } from "react-native";
+
+import { icons } from "../../constants";
+
+const SearchInput = ({
+  title,
+  value,
+  placeholder,
+  handleChangeText,
+  otherStyles,
+  ...props
+}) => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  return (
+      <View className="w-full h-16 px-4 bg-black-100 rounded-2xl border-2 border-black-200 focus:border-secondary flex flex-row items-center space-x-4">
+        <TextInput
+          className="text-base mt-0.5 text-white flex-1 font-pregular"
+          value={value}
+          placeholder={'Search for a video topic'}
+          placeholderTextColor="#7B7B8B"
+          onChangeText={handleChangeText}
+          secureTextEntry={title === "Password" && !showPassword}
+          {...props}
+        />
+
+        <TouchableOpacity>
+            <Image
+            source={icons.search}
+            className = {'w-5 h-5'}
+            resizeMode="contain"
+            />
+        </TouchableOpacity>
+      </View>
+    
+  );
+};
+
+export default SearchInput;
