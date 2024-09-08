21e933d0a5786275fa7efb942d7eb99f1b5c1c89
umerzulfiqar021 <umerzulfiqar38@gmail.com>
2024-09-08 14:40:19 +0500

    created sign in and Sign-Up screens

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -3,17 +3,21 @@ import { View, Text, ScrollView, Image} from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import {images} from '../../constants'
 import FormField from '../components/FormField'
-
+import CustomButton from '../components/CustomButton'
+import { Link } from 'expo-router';
 const SignIn = () => {
   const  [form,setForm] = useState ({
     email : "" ,
     password: ""
   });
-  
+  const [isSubmitting, setIsSubmitting] = useState (false)
+  const submit = () => {
+
+  }
   return (
    <SafeAreaView className = {'bg-primary h-full '}>
     <ScrollView>
-      <View className = {' w-full justify-center  h-full px-4 my-6'} >
+      <View className = {' w-full justify-center  min-h-[83vh] px-4 my-6'} >
     <Image source={images.logo}
     resizeMode='contain'
     className = {'w-[115px] h-[35px]'}
@@ -37,8 +41,22 @@ const SignIn = () => {
           password : e
         })}
         otherStyles = 'mt-7'
-        
         />
+      <CustomButton
+      title={'Sign In'}
+      handlePress = {submit}
+      containerStyles  = "mt-7"
+      isLoading = {isSubmitting}
+      />
+    <View className = {'justify-center pt-5 flex-row gap-2'}>
+
+      <Text className = {'text-lg text-gray-100 font-pregular'}>
+        Don't have an account?
+      </Text>
+        <Link href= 'sign-up' className = 'text-lg font-psemibold text-secondary' >
+        Sign Up
+        </Link>
+    </View>
       </View>
       
     </ScrollView>
--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,12 +1,77 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { useState } from 'react';
+import { View, Text, ScrollView, Image} from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import {images} from '../../constants'
+import FormField from '../components/FormField'
+import CustomButton from '../components/CustomButton'
+import { Link } from 'expo-router';
+const SignUp = () => {
+  const  [form,setForm] = useState ({
+    username: '',
+    email : "" ,
+    password: ""
+  });
+  const [isSubmitting, setIsSubmitting] = useState (false)
+  const submit = () => {
 
-const SignUP = () => {
+  }
   return (
-    <View>
-      <Text>SignUP</Text>
+   <SafeAreaView className = {'bg-primary h-full '}>
+    <ScrollView>
+      <View className = {' w-full justify-center  min-h-[83vh] px-4 my-6'} >
+    <Image source={images.logo}
+    resizeMode='contain'
+    className = {'w-[115px] h-[35px]'}
+    />
+    <Text className = {'text-2xl text-white text-semibold mt-10 font-psemibold'}>
+        Sign up to Aora
+        </Text>
+        <FormField
+        title = "Username"
+        value = {form.username}
+        handleChangeText = {(e)=> setForm({...form,
+          username : e
+        })}
+        otherStyles = 'mt-10'
+        
+        />
+        <FormField
+        title = "Email"
+        value = {form.email}
+        handleChangeText = {(e)=> setForm({...form,
+          email : e
+        })}
+        otherStyles = 'mt-7'
+        keyboardType = "email-address"
+        />
+        <FormField
+        title = 'Password'
+        value = {form.password}
+        handleChangeText = {(e)=> setForm({...form,
+          password : e
+        })}
+        otherStyles = 'mt-7'
+        />
+      <CustomButton
+      title={'Sign In'}
+      handlePress = {submit}
+      containerStyles  = "mt-7"
+      isLoading = {isSubmitting}
+      />
+    <View className = {'justify-center pt-5 flex-row gap-2'}>
+
+      <Text className = {'text-lg text-gray-100 font-pregular'}>
+        Have an account already?
+      </Text>
+        <Link href= 'sign-in' className = 'text-lg font-psemibold text-secondary' >
+        Sign In
+        </Link>
     </View>
+      </View>
+      
+    </ScrollView>
+   </SafeAreaView>
   )
 }
 
-export default SignUP
+export default SignUp
