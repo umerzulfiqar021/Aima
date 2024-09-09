d4e5fbc43ab0aed054d145141dc4ca71e64b43c4
umerzulfiqar021 <umerzulfiqar38@gmail.com>
2024-09-09 23:21:02 +0500

    on refresh to get latest videos in home screen

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,13 +1,20 @@
-import { View, Text, FlatList, Image } from 'react-native'
+import { View, Text, FlatList, Image, RefreshControl } from 'react-native'
 import React from 'react'
+import { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import {images} from '../../constants'
 import SearchInput from '../components/SearchInput'
 import Trending from '../components/Trending'
 import EmptyState from '../components/EmptyState'
 const Home = () => {
+  const [refreshing, setRefreshing] = useState(false)
+  const onRefresh = async () => {
+    setRefreshing(true);
+    //to get latest videos
+    setRefreshing(false);
+  } 
   return (
-    <SafeAreaView className = {'bg-primary'}>
+    <SafeAreaView className = {'bg-primary h-full'}>
     <FlatList
     // data={[{id:1},{id:2},{id:3}]}
     keyExtractor={(item) => item.$id}
@@ -49,12 +56,13 @@ const Home = () => {
     )}
     ListEmptyComponent={()=> (
      <EmptyState
-     title = "no Videos found"
+     title = "No Videos found"
      subtitle = "Be the first one to upload a video"
      />
     )}
+    refreshControl={<RefreshControl  refreshing = {refreshing} onRefresh={onRefresh} />}
     />      
-
+    
 
     </SafeAreaView>
   )
--- app/components/EmptyState.jsx
+++ app/components/EmptyState.jsx
@@ -1,7 +1,9 @@
 import { View, Text, Image } from 'react-native'
 import React from 'react'
+import { router } from 'expo-router'
 import { images } from '../../constants'
-const EmptyState = () => {
+import CustomButton from './CustomButton'
+const EmptyState = ({title,subtitle}) => {
   return (
     <View className = {'justify-center items-center px-4'}>
      <Image
@@ -9,6 +11,18 @@ const EmptyState = () => {
      className = {'w-[270px] h-[215px] '}
      resizeMode='contain'
      />
+     <Text className = {'text-xl  text-center font-psemibold  text-white mt-2'}>
+              {title}
+            </Text>
+ <Text className = {'font-pmedium text-sm text-gray-100'}>
+                {subtitle}
+            </Text>
+            
+        <CustomButton
+        title={'Create video'}
+        handlePress={()=> router.push('./create')}
+        containerStyles={'w-full my-5'}
+        />
     </View>
   )
 }
