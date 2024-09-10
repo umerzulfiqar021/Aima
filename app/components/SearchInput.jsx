c19787991b26e6739a2640950081392459aac86e
umerzulfiqar021 <umerzulfiqar38@gmail.com>
2024-09-11 00:39:07 +0500

    3:10 search screen

--- app/components/SearchInput.jsx
+++ app/components/SearchInput.jsx
@@ -1,31 +1,33 @@
 import { useState } from "react";
-import { View, Text, TextInput, TouchableOpacity, Image } from "react-native";
+import { View, Text, TextInput, TouchableOpacity, Image, Alert } from "react-native";
 
 import { icons } from "../../constants";
+import { router, usePathname } from "expo-router";
 
-const SearchInput = ({
-  title,
-  value,
-  placeholder,
-  handleChangeText,
-  otherStyles,
-  ...props
-}) => {
-  const [showPassword, setShowPassword] = useState(false);
+const SearchInput = () => {
+    const pathname = usePathname()
+    const [query,setQuery] = useState ('') 
 
   return (
       <View className="w-full h-16 px-4 bg-black-100 rounded-2xl border-2 border-black-200 focus:border-secondary flex flex-row items-center space-x-4">
         <TextInput
           className="text-base mt-0.5 text-white flex-1 font-pregular"
-          value={value}
+          value={query}
           placeholder={'Search for a video topic'}
-          placeholderTextColor="#7B7B8B"
-          onChangeText={handleChangeText}
-          secureTextEntry={title === "Password" && !showPassword}
-          {...props}
+          placeholderTextColor="#CDCDEO"
+          onChangeText={(e) => setQuery(e)}
+         
         />
 
-        <TouchableOpacity>
+        <TouchableOpacity
+        onPress={()=>{
+          if(!query){
+            return Alert.alert('Missing Query', 'Please input something to search results across database')}
+            if(pathname.startsWith('./search') )router.setParams({query})
+              else router.push (`/search/${query}`)
+          
+        }}
+        >
             <Image
             source={icons.search}
             className = {'w-5 h-5'}
--- app/search/[query].jsx
+++ app/search/[query].jsx
@@ -1,10 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-
+import { useLocalSearchParams } from 'expo-router'
 const Search = () => {
+  const {query} = useLocalSearchParams()
   return (
     <View>
-      <Text>Search</Text>
+      <Text className = 'text-3xl text-white'>{query}</Text>
     </View>
   )
 }
