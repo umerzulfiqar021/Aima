be1972f1f4edcf9ac1e77daab99bec4a6f5cd6a9
umerzulfiqar021 <umerzulfiqar38@gmail.com>
2024-09-15 00:04:17 +0500

    thumbail of image

--- app/(tabs)/create.jsx
+++ app/(tabs)/create.jsx
@@ -1,8 +1,10 @@
-import { View, Text, ScrollView } from 'react-native'
+import { View, Text, ScrollView, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
 import { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import FormField from '../components/FormField'
+import { Video, ResizeMode } from 'expo-av'
+import { icons } from '../../constants'
 const Create = () => {
   const [uploading,setUploading] = useState (false);
   const [form,setForm] = useState ({
@@ -30,7 +32,36 @@ const Create = () => {
     <Text className = 'text-base text-gray-100 font-pmedium  '>
     Upload Video
     </Text>
+    <TouchableOpacity>
+      {
+        form.video ? (
+          <Video
+          source={ {uri: form.video.uri}}
+          className = 'w-full h-64 rounded-2xl '
+          useNativeControls
+          resizeMode= {ResizeMode.COVER}
+          isLooping
 
+          />
+        ) : (
+          <View className = 'w-full h-40 px-4 bg-black-100 rounded-2xl justify-center items-center'>
+            <View className = 'w-14 h-14 border border-dashed border-secondary-100 justify-center items-center'>
+            <Image 
+            source={icons.upload}
+            resizeMode='contain'
+            className = 'w-1/2 h-1/2'
+            />
+            </View>
+          </View>
+        )
+      }
+
+    </TouchableOpacity>
+  </View>
+  <View className = 'mt-7 space-y-2'>
+  <Text className = 'text-base text-gray-100 font-pmedium  '>
+    Thumbnail  Image
+    </Text>
   </View>
 </ScrollView>
     </SafeAreaView>
